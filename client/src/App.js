086fbcfa660cbd82be12bd62b6e44d31e61e77e4
Wechuli <wechulipaul@yahoo.com>
2020-04-27 16:01:08 +0300

    adding base pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,17 @@
-import React from 'react';
-import { Switch } from 'react-router-dom';
-
+import React from "react";
+import { Switch } from "react-router-dom";
 
 function App() {
   return (
-<>
-
-<Switch>
-  
-</Switch>
-
-</>
+    <>
+      <Switch></Switch>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eveniet
+        aliquam adipisci fuga maxime illum recusandae at nisi id temporibus iure
+        quisquam sit delectus quaerat, accusantium quibusdam nobis
+        necessitatibus dolor ipsa.
+      </p>
+    </>
   );
 }
 
